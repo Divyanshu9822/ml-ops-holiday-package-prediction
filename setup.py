9d2735262e56c0b13997b81abf45915a8c8727d6
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-09-08 01:15:29 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "ml-ops-holiday-package-prediction"
+AUTHOR_USER_NAME = "Divyanshu9822"
+PROJECT_NAME = "holiday_package_prediction"
+AUTHOR_EMAIL = "divyanshu9871@gmail.com"
+
+
+setuptools.setup(
+    name=PROJECT_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
